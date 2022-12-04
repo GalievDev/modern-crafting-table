c17eac6f5e250b14e6024e2b6a179765e25306ee
GalievDev <ggvalion@gmail.com>
2022-12-04 22:07:26 +0500

    Initial commit

---
+++ src/main/java/net/galievdev/moderncraftingtable/ModernCraftingTable.java
@@ -0,0 +1,4 @@
+package net.galievdev.moderncraftingtable;
+
+public class ModernCraftingTable {
+}
---
+++ src/main/java/net/galievdev/moderncraftingtable/client/ModernCraftingTableClient.java
@@ -0,0 +1,5 @@
+package net.galievdev.moderncraftingtable.client;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class ModernCraftingTableClient {
+}
